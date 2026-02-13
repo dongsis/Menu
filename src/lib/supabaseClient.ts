1ad3b18c3b88f70712bbf42db31b7369b66b6fab
dongsis <beautfly@gmail.com>
2026-02-12 19:26:28 -0500

    debug env in build

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,13 @@
 console.log(
   "ENV check",
   "URL?", !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+  "URL_LEN", process.env.NEXT_PUBLIC_SUPABASE_URL?.length ?? 0,
+  "ANON?", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  "ANON_LEN", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.length ?? 0
+);
+console.log(
+  "ENV check",
+  "URL?", !!process.env.NEXT_PUBLIC_SUPABASE_URL,
   "ANON?", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 import { createClient } from "@supabase/supabase-js";
